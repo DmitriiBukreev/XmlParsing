7537e90627907204f61f636fce95d7571b42c952
Dmitrii Bukreev <1stringsamurai@gmail.com>
2022-03-21 16:25:10 +0100

    Second commit

--- src/main/java/dima/StartApplication.java
+++ src/main/java/dima/StartApplication.java
@@ -1,7 +1,22 @@
 package dima;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import mypackage.TextTablesType;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class StartApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+        JAXBContext jaxbContext   = JAXBContext.newInstance( TextTablesType.class );
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        InputStream inStream = new FileInputStream( "german.xml" );
+
+        TextTablesType textTablesType = (TextTablesType) jaxbUnmarshaller.unmarshal( inStream );
 
     }
 }
