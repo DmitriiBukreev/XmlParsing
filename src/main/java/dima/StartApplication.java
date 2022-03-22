e74d9cbb3771f593dc305ec578789d312ec6d3e7
Dmitrii Bukreev <1stringsamurai@gmail.com>
2022-03-22 13:12:59 +0100

    DB config

--- src/main/java/dima/StartApplication.java
+++ src/main/java/dima/StartApplication.java
@@ -9,15 +9,39 @@ import mypackage.TextTablesType;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class StartApplication {
+    private final String url = "jdbc:postgresql://localhost:5432/german_db";
+    private final String user = "postgres";
+    private final String password = "98a2c94d";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+
     public static void main(String[] args) throws JAXBException, FileNotFoundException {
         JAXBContext jaxbContext = JAXBContext.newInstance(TextTablesType.class);
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+
         InputStream inStream = new FileInputStream("german.xml");
         TextTablesType textTablesType = (TextTablesType) jaxbUnmarshaller.unmarshal(inStream);
         textTablesType.getTextTable().getText();
-
+        StartApplication startApplication =new StartApplication();
+        startApplication.connect();
 
     }
 }
